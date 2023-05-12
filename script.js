1c186a56cef1a9536aee8475fea8e9ec79452635
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-05-12 11:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
+Person.prototype.greet=function () {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
+function Employee(name, age, jobTitle) {
+	this.name=name;
+	this.age=age;
+	this.jobTitle=jobTitle;
+}
 
-function Employee(name, age, jobTitle) {}
+Employee.prototype = Object.create(Person.prototype);
 
+
+Employee.prototype.jobGreet=function()
+	{
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
